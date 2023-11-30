0bbe2826ece581535bcc6cc115ffb6c8533417ce
NIN VITOU <134988087+zTouu@users.noreply.github.com>
2023-11-30 16:47:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,10 +28,11 @@ fetch(`${url}${inputword}`)
     <p class="word-example">
         ${data[0].meanings[0].definitions[0].example || ""}
     </p>`;
-    sound.setAttribute("src", data[0].phonetics[0].audio); //Already fixed, the old codes was => "sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);"
+        sound.setAttribute("src", data[0].phonetics[0].audio); //Already fixed, the old codes was => "sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);"
     });
 
 });
+
 function playSound(){
     sound.play();
 }
