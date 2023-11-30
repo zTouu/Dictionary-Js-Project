46bb1c8106eb518b5cfeccc5399cd928889f50cd
NIN VITOU <134988087+zTouu@users.noreply.github.com>
2023-11-30 16:44:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,25 +11,25 @@ fetch(`${url}${inputword}`)
 .then((response) => response.json())
 .then((data) => {
 
-result.innerHTML = `
-<div class="word">
-<h3>${inputword}</h3>
-<button onclick="playSound()">
-    <i class="fas fa-volume-up"></i>
-</button>
-</div>
-<div class="details">
-<p>${data[0].meanings[0].partOfSpeech}</p>
-<p>/${data[0].phonetic}/</p>
-</div>
-<p class="word-meaning">
-${data[0].meanings[0].definitions[0].definition}
-</p>
-<p class="word-example">
-${data[0].meanings[0].definitions[0].example || ""}
-</p>`;
-sound.setAttribute("src", data[0].phonetics[0].audio); //Already fixed, the old codes was => "sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);"
-});
+    result.innerHTML = `
+    <div class="word">
+    <h3>${inputword}</h3>
+    <button onclick="playSound()">
+        <i class="fas fa-volume-up"></i>
+    </button>
+    </div>
+    <div class="details">
+        <p>${data[0].meanings[0].partOfSpeech}</p>
+        <p>/${data[0].phonetic}/</p>
+    </div>
+    <p class="word-meaning">
+        ${data[0].meanings[0].definitions[0].definition}
+    </p>
+    <p class="word-example">
+        ${data[0].meanings[0].definitions[0].example || ""}
+    </p>`;
+    sound.setAttribute("src", data[0].phonetics[0].audio); //Already fixed, the old codes was => "sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);"
+    });
 
 });
 function playSound(){
